ba4d511e34b5b0dd6d08c7168f5be6cd5bc0bfc1
Emily Hoult <emily@emilyhoult.com>
2021-05-17 16:32:08 +0200

    update game with messages

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word].upcase
+    @letters = params[:letters].split
+    if in_grid?(@word, @letters) == false
+      @message = "Sorry but #{@word} can't be built out of #{@letters.each { |l| puts l }},"
+    elsif in_grid?(@word, @letters) == true && in_dict?(@word) == false
+      @message = "Sorry but #{@word} does not seem to be a valid English word..."
+    else
+      @message = "Congratulations! #{@word} is a valid English word!"
+    end
+  end
+
+  def in_grid?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def in_dict?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
